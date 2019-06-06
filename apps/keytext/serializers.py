34ab7008cfea4960b3842cae41dc0293155e7710
Ivan Iurii <iuriiiv302@gmail.com>
2019-06-06 18:24:37 +0300

    add validation
    Signed-off-by: Ivan Iurii <iuriiiv302@gmail.com>

--- apps/keytext/serializers.py
+++ apps/keytext/serializers.py
@@ -5,6 +5,14 @@ from apps.keytext.models import KeyWord
 
 
 class TextkeySerializer(serializers.ModelSerializer):
+    key = serializers.CharField()
+
+    def validate_key_search(self, object):
+        key = KeyWord.objects.filter(key=object)
+        if not key:
+            raise ValidationError("Not key")
+        return object
+
     class Meta:
         model = KeyWord
         fields = ('key',)
--- apps/keytext/views.py
+++ apps/keytext/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from drf_util.decorators import serialize_decorator
 from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView
@@ -15,11 +16,15 @@ class TextFilterKey(GenericAPIView):
 
     @serialize_decorator(TextkeySerializer)
     def post(self, request):
-        validated_data = request.serializer.validated_data
-        print(validated_data)
-        text = KeyWord.objects.filter(key=validated_data['key'])
-        response_data = TextkeyAllSerializer(text, many=True).data
-        return Response(response_data)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            validated_data = request.serializer.validated_data
+            text = KeyWord.objects.filter(key=validated_data['key'])
+            response_data = TextkeyAllSerializer(text, many=True).data
+            return Response(response_data)
+        else:
+            return JsonResponse({'key':'incorrect'})
+
 
     def get(self,request):
         text = KeyWord.objects.order_by('key')
